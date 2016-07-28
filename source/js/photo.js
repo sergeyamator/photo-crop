373c2b289019cbdf2adabce349c22f5d52dc16a6
Sergey Rubets <sramator@gmail.com>
2016-07-28 12:32:01 +0300

    Drag and drop functionality was added

--- source/js/photo.js
+++ source/js/photo.js
@@ -15,21 +15,19 @@ class Photo {
    */
   setImage(file) {
     let fileReader = new FileReader();
-
     this.element.classList.remove('is-hidden');
 
     fileReader.addEventListener('load', () => {
       this.img.src = fileReader.result;
       this._createCropArea(this.img.offsetWidth / 2);
+      this._allowDraggable(this.cropArea);
     });
-
 
     fileReader.readAsDataURL(file);
   }
 
-  _renderCropView() {
-
-
+  _allowDraggable(el) {
+    el.addEventListener('mousedown', _onMouseDown);
   }
 
   crop() {
@@ -40,14 +38,71 @@ class Photo {
     // TODO
   }
 
+  /**
+   * Create crop area and add it to the page
+   * @param {Number} width Width and height crop area
+   * @private
+   */
   _createCropArea(width) {
-    this.cropArea = document.createElement('div');
-    this.cropArea.classList.add('crop-area');
+    if (document.querySelector('.crop-area')) {
+      this.cropArea = document.querySelector('.crop-area');
+      this.cropArea.style.width = width + 'px';
+      this.cropArea.style.height = width + 'px';
+    } else {
+      this.cropArea = document.createElement('div');
+      this.cropArea.classList.add('crop-area');
+      this.cropArea.style.width = width + 'px';
+      this.cropArea.style.height = width + 'px';
 
-    this.cropArea.style.width = width + 'px';
-    this.cropArea.style.height = width + 'px';
+      document.querySelector('.picture-crop-area').appendChild(this.cropArea);
+    }
+  }
+}
 
-    document.querySelector('.picture-section').appendChild(this.cropArea);
+
+function _onMouseDown(e) {
+  this.activeEl = e.target;
+  this._offsetX = e.offsetX;
+  this._offsetY = e.offsetY;
+
+  document.body.addEventListener('mousemove', _onMouseMove.bind(this));
+  document.body.addEventListener('mouseup', _onMouseUp.bind(this));
+}
+
+function _onMouseUp() {
+  this.activeEl = null;
+}
+
+function _onMouseMove(e) {
+  if (this.activeEl) {
+    this.activeEl.style.transform = 'translate(0, 0)';
+    let parent = document.querySelector('.picture-crop-area'),
+      parentX = parent.getBoundingClientRect().left,
+      parentY = parent.getBoundingClientRect().top,
+      maxRightCoord = parent.offsetWidth - this.activeEl.offsetWidth,
+      maxBottomCoord = parent.offsetHeight - this.activeEl.offsetHeight;
+
+    let leftCoord = e.clientX - this._offsetX - parentX,
+      topCoord = e.clientY - this._offsetY - parentY;
+
+    if (leftCoord <= 0) {
+      leftCoord = 0;
+    }
+
+    if (topCoord <= 0) {
+      topCoord = 0;
+    }
+
+    if (leftCoord >= maxRightCoord) {
+      leftCoord = maxRightCoord;
+    }
+
+    if (topCoord >= maxBottomCoord) {
+      topCoord = maxBottomCoord;
+    }
+
+    this.activeEl.style.left = leftCoord + 'px';
+    this.activeEl.style.top = topCoord + 'px';
   }
 }
 
